8820f846c817f55e86f6046837e33295de8d2570
Vo Hong Phong <phong.vo@fiisoft.net>
2023-04-20 17:32:59 +0700

    first commit

---
+++ background.js
@@ -0,0 +1,15 @@
+﻿let value;
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.method) {
+    case "send":
+      console.log("method: " + message.method);
+      value = message.value;
+      break;
+    case "recv":
+      sendResponse({ value: value });
+      break;
+  }
+
+  return true;
+});
---
+++ myscript.js
@@ -0,0 +1,33 @@
+﻿// document.getElementById("id_Recv").onclick = () => {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     chrome.scripting.executeScript({
+//       target: { tabId: tabs[0].id },
+//       func: RecvFromDomainA
+//     });
+//   });
+// }
+
+
+document.getElementById("id_run").onclick = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: SendToDomainB
+    });
+  });
+}
+
+// function RecvFromDomainA() {
+//   let value = document.getElementById("Icname").value;
+//   cachesonsole.log("value: "+ value);
+//   chrome.runtime.sendMessage({ method: "send", key: "key", value: value }, () => {
+//   });
+// }
+
+function SendToDomainB() {
+  chrome.runtime.sendMessage({ method: "recv", key: "key" }, (response) => {
+    let x = document.getElementsByTagName("input")[2].value;
+    console.log("value1: " + x);
+  });
+}
+// /html/body/div[7]/div[2]/div[1]/colab-tab-layout-container/div/div/colab-tab-pane/div/div[2]/colab-tab/div/colab-shaded-scroller/div[1]/div[1]/div/div[2]/div[1]/div[2]/div[2]/div[1]/colab-form/div/colab-form-input[1]/div[2]/paper-input//paper-input-container/iron-input
